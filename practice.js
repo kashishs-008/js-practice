3a8d399f131e9e80cb8db5efe82a67a803148b9a
Kashish <saharankashish9@gmail.com>
2026-06-25 05:31:18 +0530

    Adding more functions for practice

--- practice.js
+++ practice.js
@@ -1,7 +1,16 @@
+//Printing
 console.log("Javascript Practice");
 
+//Using Functions
+//addition
 function add(a, b) {
   return a + b;
 }
-
+//subtraction 
 console.log(add(2, 3));
+
+function subtract(a, b) {
+    return a - b;
+}
+
+console.log(subtract(10, 5)); 
