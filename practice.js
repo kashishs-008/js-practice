bfb5e888f80fd3b24a729617f9ec727c61a0ad98
Kashish <saharankashish9@gmail.com>
2026-06-25 05:47:45 +0530

    adding functions

--- practice.js
+++ practice.js
@@ -12,3 +12,28 @@ function subtract(a, b) {
     return a - b;
 }
 console.log(subtract(10, 5)); 
+//multiplication
+function multiply(a, b) {
+    return a * b;
+}
+console.log(multiply(10, 5)); 
+//division
+function divide(a, b) {
+    return a / b;
+}
+console.log(divide(10, 5)); 
+//modulus
+function modulus(a, b) {
+    return a % b;
+}
+console.log(modulus(10, 3)); 
+//power
+function power(a, b) {
+    return a ** b;
+}
+console.log(power(2, 3)); 
+//square root
+function squareRoot(num) {
+    return Math.sqrt(num);
+}
+console.log(squareRoot(25)); 
